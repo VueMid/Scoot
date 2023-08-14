9eb226602d739fbde4f377f1e7dee2fe6ba35c18
Muslim-571 <turabovumid587@gmail.com>
2023-08-14 15:05:12 +0500

    asdasd

--- src/MaterialUI/TheButton.vue
+++ src/MaterialUI/TheButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="bg-darker-yellow duration-1000 py-[14px] px-[39px] border-[3px] border-darker-yellow text-white font-bold text-[15px] active:bg-inherit active:text-darker-yellow active:duration-1000">
+        class="bg-darker-yellow py-[14px] px-[39px] border-[3px] border-darker-yellow text-white font-bold text-[15px] active:bg-inherit active:text-darker-yellow">
         <slot />
     </button>
 </template>
