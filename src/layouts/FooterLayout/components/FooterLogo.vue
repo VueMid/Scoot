84348f060010560c4057673642abf302882d4e5b
Muslim-571 <turabovumid587@gmail.com>
2023-08-31 19:06:31 +0500

    fix:Some bugs of Home-page were fixed very well(08.31.2023/19:07)

--- src/layouts/FooterLayout/components/FooterLogo.vue
+++ src/layouts/FooterLayout/components/FooterLogo.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="footer__image-box mt-16 cursor-pointer duration-100 active:opacity-70 md:mt-0"
-        @click="$router.push('/')">
+    <div class="footer__image-box mt-16 cursor-pointer duration-100 active:opacity-70 md:mt-0" @click="$router.push('/')">
         <img class="footer__logo" src="@/assets/icons/logo__second-image.svg" alt="logo__image">
     </div>
 </template>
--- src/layouts/HeaderLayout/components/HeaderLogo.vue
+++ src/layouts/HeaderLayout/components/HeaderLogo.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="header__logo w-[76px] md:w-auto cursor-pointer duration-100 active:opacity-70"
-        @click="$router.push('/')">
+    <div class="header__logo w-[76px] md:w-auto cursor-pointer duration-100 active:opacity-70" @click="$router.push('/')">
         <img class="block dark:hidden" src="@/assets/icons/logo__image.svg" alt="logo__image">
         <img class="hidden dark:block" src="@/assets/icons/logo__second-image.svg" alt="logo__image">
     </div>
